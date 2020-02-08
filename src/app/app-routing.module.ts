ff78d328505873a910c4b635e219bfe83feee8e5
Aqvarion <black.game.apple789@mail.ru>
2020-02-08 23:56:33 +0300

    Upp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CatalogComponent} from './components/catalog/catalog.component';
+import {UserLoginComponent} from './components/user-login/user-login.component';
+import {UserRegistrationComponent} from './components/user-registration/user-registration.component';
+import {BookDetailsComponent} from './components/book-details/book-details.component';
+import {ShoppingCartComponent} from './components/shopping-cart/shopping-cart.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'catalog', component: CatalogComponent},
+  { path: 'registration', component: UserRegistrationComponent},
+  { path: 'login', component: UserLoginComponent},
+  { path: 'book-details/:id', component: BookDetailsComponent},
+  { path: 'shop-cart', component: ShoppingCartComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TopBarComponent} from './components/top-bar/top-bar.component';
+import {CatalogComponent} from './components/catalog/catalog.component';
+import {UserRegistrationComponent} from './components/user-registration/user-registration.component';
+import {UserLoginComponent} from './components/user-login/user-login.component';
+import {HttpClientModule} from '@angular/common/http';
+import {BookDetailsComponent} from './components/book-details/book-details.component';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatOptionModule, MatSelectModule} from '@angular/material';
+import {BookService} from './service/book.service';
+import {ShoppingCartComponent} from './components/shopping-cart/shopping-cart.component';
+import {LocalStorageService, StorageService} from './service/storage.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    CatalogComponent,
+    UserRegistrationComponent,
+    UserLoginComponent,
+    BookDetailsComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatOptionModule,
+    MatSelectModule
   ],
-  providers: [],
+  providers: [
+    BookService,
+    LocalStorageService,
+    { provide: StorageService, useClass: LocalStorageService}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
